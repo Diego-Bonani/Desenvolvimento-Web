226fe45563b54419b1e43afc90f3433e348ec09c
DIEGO GODE BONANI <202401000711@alunos.ibmec.edu.br>
2024-11-01 17:01:53 -0300

    resto da aula

--- Aula_8/codigo.js
+++ Aula_8/codigo.js
@@ -1,20 +1,45 @@
 
+const url = 'https://botafogo-atletas.mange.li/2024-1/';
+
+const pega_json = async(caminho) => {
+    const resposta = await fetch(caminho);
+    const dados = await resposta.json();
+    return dados;
+}
+
 const container = document.getElementById("container");
 
-let conteudo;
 
-const montaCard = (atleta) => `
-    <article>
-        <h1>${atleta.nome}</h1>
-        <img src="${atleta.imagem}">
-        <p>${atleta.detalhes}</p>
-    </article>
-`
-dados.forEach(
-    (ele) => conteudo += montaCard(ele)
-)
+const montaCard = (atleta) => {
+    const cartao = document.createElement("article");
+    const nome = document.createElement("h1");
+    const imagem = document.createElement("img");
+    const descri = document.createElement("p");
+    const link = document.createElement('a');
+
+    nome.innerHTML = atleta.nome;
+    nome.style.fontFamily = "sans-serif"
+    cartao.appendChild(nome);
+
+    imagem.src = atleta.imagem;
+    cartao.appendChild(imagem);
+
+    descri.innerHTML = atleta.detalhes;
+    cartao.appendChild(descri);
+
+    link.innerText = "Saiba mais..."
+    link.href = `detalhes.html?id=${atleta.id}`;
+    cartao.appendChild(link)
 
 
+    return cartao;
+};        
 
-container.innerHTML = conteudo;
+pega_json(`${url}masculino`).then(
+    (r) => {
+        r.forEach(
+            (ele) => container.appendChild(montaCard(ele))
+        )
+    }
+);
 
